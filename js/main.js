fb051f8d484456ee3a594901dbfd91f92ad8eb32
DenNTRooT <DenNTRooT@gmail.com>
2014-09-11 20:12:59 +0600

    JS border radius

--- js/main.js
+++ js/main.js
@@ -1,1 +1,53 @@
+(function(){
 
+    // переменые
+    var increaseRad = $('#increase-radius'),
+        reduceRad = $('#reduce-radius'),
+        create = $('.create'),
+        maxRadius = 20,
+        minRadius = 0;
+
+    increaseRad.on('click', function(){
+
+        var currentRadius = create.css('border-radius'),
+            step = $('#step').val(),
+            newRadius = (parseInt(currentRadius) + parseInt(step));
+
+        if (newRadius > maxRadius) {
+            newRadius = maxRadius;
+            $(this).addClass('disabled');
+        }
+
+        if (newRadius > minRadius) {
+            reduceRad.removeClass('disabled');
+        }
+
+        create.css({
+            'border-radius' : newRadius
+        })
+
+    });
+
+    reduceRad.on('click', function(){
+
+        var currentRadius = create.css('border-radius'),
+            step = $('#step').val(),
+            newRadius = (parseInt(currentRadius) - parseInt(step));
+
+        if (newRadius < minRadius) {
+            newRadius = minRadius;
+            $(this).addClass('disabled');
+        }
+
+        if (newRadius < maxRadius) {
+            increaseRad.removeClass('disabled');
+        }
+
+        create.css({
+            'border-radius' : newRadius
+        })
+
+    });
+
+
+}());
